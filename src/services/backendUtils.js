d1e32d98476640af2b68ade86f89977abba0af80
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-10-07 16:23:35 -0700

    built getById and updateById functions in backendUtils

--- src/services/backendUtils.js
+++ src/services/backendUtils.js
@@ -12,3 +12,28 @@ export async function createUser(user) {
   );
   return response.body;
 }
+
+export async function getUserById(id) {
+  const response = await fetch(`${URL}/api/v1/users/${id}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }
+  );
+  return response.body;
+}
+
+export async function updateUserById(id, updatedUser) {
+  const response = await fetch(`${URL}/api/v1/users/${id}`,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedUser)
+    }
+  );
+  return response.body;
+}
