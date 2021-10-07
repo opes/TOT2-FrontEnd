09948c55e455b9106591daa40ef2146d4dea180c
Missael Ortiz <ortiz.missael80@gmail.com>
2021-10-07 16:06:55 -0700

    added hero starting stats to hero-templates.js

---
+++ src/data/hero-templates.js
@@ -0,0 +1,45 @@
+const dwarfWarrior = {
+  HP: 25,
+  STM: 10,
+  AC: 6,
+  SPD: 1,
+  ATK: 3,
+  level: 1,
+  gold: 0,
+  XP: 0
+};
+
+const foxArcher = {
+  HP: 20,
+  STM: 10,
+  AC: 2,
+  SPD: 5,
+  ATK: 4,
+  level: 1,
+  gold: 0,
+  XP: 0
+};
+
+const devilkinMage = {
+  HP: 15,
+  STM: 10,
+  AC: 1,
+  SPD: 6,
+  ATK: 6,
+  level: 0,
+  gold: 0,
+  XP: 0
+};
+
+const vampireKnight = {
+  HP: 30,
+  STM: 10,
+  AC: 3,
+  SPD: 3,
+  ATK: 3,
+  level: 1,
+  gold: 0,
+  XP: 0
+};
+
+export const heroes = { dwarfWarrior, foxArcher, devilkinMage, vampireKnight };
