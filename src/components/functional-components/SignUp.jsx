e38a3016153996dad65a233c6c221a462afcac4a
Missael Ortiz <ortiz.missael80@gmail.com>
2021-10-07 11:43:43 -0700

    WIP: google token successful stored in state, munged username and googleId off token, need to work on user form and onSubmit func

--- src/components/functional-components/SignUp.jsx
+++ src/components/functional-components/SignUp.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useState, useEffect } from 'react';
 import GoogleLogin from 'react-google-login';
 
 const SignUp = () => {
-// THIS IS WHERE OUR HOOK GOES
+  const [token, setToken] = useState();
+  const [username, setUsername] = useState();
+  const [googleId, setGoogleId] = useState();
+  const [hero, setHero] = useState();
 
+  useEffect(() => {
+    setGoogleId(token?.googleId);
+    const protoUsername = `${token?.profileObj.givenName}${token?.profileObj.familyName[0]}`;
+    setUsername(protoUsername);
+  }, [token]);
+
+  const onSubmit = async () => {
+    // this is where we create a userObject to be posted to the backend.
+    // this also redirects the user to the first CUTSCENE after their userObject has been added to the database.
+    // this is the start of the app.
+  };
+
+  if(!token) return <GoogleLogin className="button"
+    clientId={process.env.CLIENT_GOOGLE_ID}
+    buttonText="Signup using Google"
+    onSuccess={ (token) => {setToken(token); } }
+    onFailure={ (response) => console.log(response)}
+    cookiePolicy={'single_host_origin'}
+  />;
   return (
     <div>
-      <GoogleLogin className="button"
-        clientId={process.env.CLIENT_GOOGLE_ID}
-        buttonText="Signup using Google"
-        onSuccess={ (token) => {console.log(token); } }
-        onFailure={ (response) => console.log(response)}
-        cookiePolicy={'single_host_origin'}
-      />
+      <form>
+        <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+        <input type="radio" name="hero" />
+        <input type="radio" name="hero" />
+        <input type="radio" name="hero" />
+        <input type="radio" name="hero" />
+        <button>Start the Adventure</button>
+      </form>
     </div>
   );
 };
