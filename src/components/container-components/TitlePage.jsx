bb012ba8ffa7a6535804b17768c42555716e85e1
Missael Ortiz <ortiz.missael80@gmail.com>
2021-10-07 10:35:10 -0700

    added init file structure

---
+++ src/components/container-components/TitlePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TitlePage = () => {
+  return (
+    <div>
+            Title Page
+    </div>
+  );
+};
+
+export default TitlePage;
---
+++ src/components/display-components/PlayerScroll.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayerScroll = () => {
+  return (
+    <div>
+      Player stats here! 
+    </div>
+  );
+};
+
+export default PlayerScroll;
