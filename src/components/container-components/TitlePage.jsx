3d380d4ebd588227212fa29fe3ae12d927b827ca
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-10-07 15:24:22 -0700

    Added in some state management for titlepage and sign up

--- src/components/container-components/TitlePage.jsx
+++ src/components/container-components/TitlePage.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
 import SignUp from '../functional-components/SignUp';
 import styles from './TitlePage.css';
 
 const TitlePage = () => {
+  const [signedIn, setSignedIn] = useState(false);
+  console.log(signedIn);
+  if(signedIn) return (<div className={styles['main-container']}><SignUp event={setSignedIn} /></div>);
   return (
     <div className={styles['main-container']}>
       <div className={styles['left-container']}>
-        <SignUp />
+        <SignUp event={setSignedIn} state={signedIn}/>
       </div>
       <div className={styles['right-container']}></div>
     </div>
