addf0b41bf7392ff82f6b54ab704b3ad47d5c82e
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-08 17:25:21 -0400

    added handleLogin so that the app will direct existing users to the village directly

--- src/components/functional-components/Login.jsx
+++ src/components/functional-components/Login.jsx
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import GoogleLogin from 'react-google-login';
-import { useSetActiveSession, useSetContextGoogleId } from '../../hooks/SessionProvider';
+import {
+  useSetActiveSession,
+  useSetContextGoogleId,
+} from '../../hooks/SessionProvider';
 import { getUserById } from '../../services/backendUtils';
 
 const Login = () => {
   const [token, setToken] = useState();
   const setContextGoogleId = useSetContextGoogleId();
   const setActiveSession = useSetActiveSession();
-  
-  // a userEffect token change
-  useEffect(() => { 
-    getUserById(token?.googleId)
-      .then((bckRes) => {
-        bckRes ?
-          (
-            setContextGoogleId(token?.googleId),
-            setActiveSession(true), 
-            location.replace('/village')
-          )
-          :
-          alert('You must create an account before logging in...')
-      });
-  }, [token]
-  )
 
+  const handleLogin = async () => {
+    const bckRes = await getUserById(token?.googleId);
+    if (bckRes) {
+      setContextGoogleId(token?.googleId);
+      setActiveSession(true);
+      location.replace('/village');
+    } else {
+      alert('You must create an account before logging in...');
+    }
+  };
 
   return (
     <div>
@@ -31,11 +28,9 @@ const Login = () => {
         className="button"
         clientId={process.env.CLIENT_GOOGLE_ID}
         buttonText="Login using Google"
-        onSuccess={(token) => { 
-          setToken(token); 
-          // setContextGoogleId(token?.googleId);
-          // setActiveSession(true);
-          // location.replace('/village')
+        onSuccess={(token) => {
+          setToken(token);
+          handleLogin();
         }}
         onFailure={(response) => console.log(response)}
         cookiePolicy={'single_host_origin'}
@@ -43,6 +38,6 @@ const Login = () => {
       ;
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
