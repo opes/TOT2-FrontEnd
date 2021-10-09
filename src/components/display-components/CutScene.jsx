880a0a4893963dbbcda8870998936e358457a096
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-08 20:04:15 -0400

    added the tutorial page

--- src/components/display-components/CutScene.jsx
+++ src/components/display-components/CutScene.jsx
@@ -1,18 +1,23 @@
 import React from 'react'; 
 import useGetUser from '../../hooks/useGetUser';
+import { useHistory } from 'react-router-dom';
 import { cutscene } from '../../data/cutscene';
 import { useContextGoogleId } from '../../hooks/SessionProvider';
 
 const CutScene = () => {
   const contextGoogleId = useContextGoogleId();
   const { userObj, loading } = useGetUser(contextGoogleId);
+  const history = useHistory(); 
 
   if (loading) return <div>'Loading...'</div>
-  console.log(userObj);
+
   return (
     <div>
       <article>{cutscene.intro}</article>
       <article>{cutscene[userObj?.heroStats?.type]}</article>
+      <button onClick={() => history.push('/tutorial')}>
+        Begin Adverture
+      </button> 
     </div>
   );
 }
---
+++ src/components/functional-components/Tutorial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tutorial = () => {
+  return (
+    <div>
+      <h1>This is the turorial page!</h1>
+    </div>
+  )
+}
+
+export default Tutorial
