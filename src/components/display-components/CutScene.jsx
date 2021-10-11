6cb992f2427e1e361e7926ccda4112e7f5673992
Missael Ortiz <ortiz.missael80@gmail.com>
2021-10-11 09:51:48 -0700

    added activeSession tracking logic; pushes user back to TitlePage if no activeSession

--- src/components/display-components/CutScene.jsx
+++ src/components/display-components/CutScene.jsx
@@ -1,14 +1,18 @@
+/* eslint-disable max-len */
 import React from 'react'; 
 import useGetUser from '../../hooks/useGetUser';
 import { useHistory } from 'react-router-dom';
 import { cutscene } from '../../data/cutscene';
-import { useContextGoogleId } from '../../hooks/SessionProvider';
+import { useActiveSession, useContextGoogleId } from '../../hooks/SessionProvider';
 
 const CutScene = () => {
   const contextGoogleId = useContextGoogleId();
+  const activeSession = useActiveSession();
   const { userObj, loading } = useGetUser(contextGoogleId);
   const history = useHistory(); 
 
+  if (!activeSession) history.push('/');
+  
   if (loading) return <div>Loading...</div>;
 
   return (
