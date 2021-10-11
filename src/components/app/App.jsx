b57fc1ac5d9c47e2645a3032d98e0f47bd9f53be
Missael Ortiz <ortiz.missael80@gmail.com>
2021-10-11 10:18:19 -0700

    added HeroProvider integration to App.js routes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,22 +3,23 @@ import TitlePage from '../container-components/TitlePage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CutScene from '../display-components/CutScene';
 import Tutorial from '../functional-components/Tutorial';
+import HeroProvider from '../../hooks/HeroProvider';
 // import '../../styles/global-styles.css';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        {/*<Hero Provider /> */}
-        <Route exact path="/cutscene">
-          <CutScene />
-        </Route>
-        <Route exact path="/tutorial">
-          <Tutorial />
-        </Route>
-        {/* VILLAGE ROUTE */}
-        {/* COMBAT ROUTE */}
-        {/*<HeroProvider /> */}
+        <HeroProvider >
+          <Route exact path="/cutscene">
+            <CutScene />
+          </Route>
+          <Route exact path="/tutorial">
+            <Tutorial />
+          </Route>
+          {/* VILLAGE ROUTE */}
+          {/* COMBAT ROUTE */}
+        </ HeroProvider>
         <Route exact path="/">
           <TitlePage />
         </Route>
