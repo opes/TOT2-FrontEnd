147540784bc1617cac11fb65e7d011b18d8f19c1
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-12 13:19:06 -0400

    added main village loginc to dynamically render church shop and tavern component

--- src/components/container-components/VillagePage.jsx
+++ src/components/container-components/VillagePage.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import { useActiveSession } from '../../hooks/SessionProvider';
 
 const VillagePage = () => {
   const activeSession = useActiveSession();
   const history = useHistory(); 
-
+  const [villageLocation, setVillageLocation] = useState('main'); 
   if (!activeSession) history.push('/'); 
+  
+  const handleVillageLocationChange = ({ target }) => {
+    setVillageLocation(target.value);
+  }
+
+  if (villageLocation === 'tavern') {
+    <Tavern />
+  } else if (villageLocation === 'shop') {
+    <Shop />
+  } else if (villageLocation === 'church') {
+    <Church />
+  }
   
   return (
     <div>
-      <button onClick={() => history.push('/cutscene')}>Cutscene</button>
+      {villageLocation}
+      <button
+        value="tavern"
+        onClick={(event) => handleVillageLocationChange(event)}
+      >
+        Tavern
+      </button>
+      <button
+        value="church"
+        onClick={(event) => handleVillageLocationChange(event)}
+      >
+        Church
+      </button>
+      <button
+        value="shop"
+        onClick={(event) => handleVillageLocationChange(event)}
+      >
+        Shop
+      </button>
     </div>
   );
 }
 
 export default VillagePage
+
+// Create a main locaiton
+  // 3 functions for each buttion
+  // if else => 
+  // create four buttons => tavern, chrunch, shop
