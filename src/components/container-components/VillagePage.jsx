add0172dd1e88eee9dcd25163fc22c9131ba76c8
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-13 18:38:38 -0400

    added styles to village component to show the map and buttons

--- src/components/container-components/VillagePage.jsx
+++ src/components/container-components/VillagePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useContextHero } from '../../hooks/HeroProvider';
 import { useActiveSession } from '../../hooks/SessionProvider';
@@ -6,38 +6,32 @@ import Church from '../display-components/Church';
 import PlayerScroll from '../display-components/PlayerScroll';
 import Shop from '../display-components/Shop';
 import Tavern from '../display-components/Tavern';
-import styles from './VillagePage.css'
+import styles from './VillagePage.css';
 // import Login from '../functional-components/Login';
-
 
 const VillagePage = () => {
   const activeSession = useActiveSession();
-  const history = useHistory(); 
-  const [villageLocation, setVillageLocation] = useState('main'); 
+  const history = useHistory();
+  const [villageLocation, setVillageLocation] = useState('main');
   const contextHero = useContextHero();
 
-  if (!activeSession) history.push('/'); 
+  if (!activeSession) history.push('/');
 
   const handleVillageLocationChange = ({ target }) => {
     setVillageLocation(target.value);
-  }
+  };
 
   const handleWilderness = () => {
-    const message = confirm('You are about to head to the wilderness, do you want to continue?')
+    const message = confirm(
+      'You are about to head to the wilderness, do you want to continue?'
+    );
     if (message) {
-      history.push('/combat')
+      history.push('/combat');
     }
-  }
+  };
 
-  // if (villageLocation === 'tavern')
-  //   return (<Tavern handleVillageLocationChange={handleVillageLocationChange} />);
-  
-  // if (villageLocation === 'shop')
-  //   return (<Shop handleVillageLocationChange={handleVillageLocationChange} />);
-  
-  // if (villageLocation === 'church')
-  //   return (<Church handleVillageLocationChange={handleVillageLocationChange} />);
-   console.log(villageLocation);
+
+  console.log(villageLocation);
   return (
     <div className={styles['village-main-container']}>
       <div className={styles['left-component-playerScroll']}>
@@ -80,37 +74,38 @@ const VillagePage = () => {
             <></>
           )}
         </div>
-        <div className={styles['village-buttons']}>
-          <div className={styles['tavern-component']}>
-            <button
-              value="tavern"
-              onClick={(event) => handleVillageLocationChange(event)}
-            >
-              Tavern
-            </button>
-          </div>
-          <div className={styles['church-component']}>
-            <button
-              value="church"
-              onClick={(event) => handleVillageLocationChange(event)}
-            >
-              Church
-            </button>
-          </div>
-          <div className={styles['shop-component']}>
-            <button
-              value="shop"
-              onClick={(event) => handleVillageLocationChange(event)}
-            >
-              Shop
-            </button>
-          </div>
-          <div className={styles['local-wilderness-component']}></div>
-          <button onClick={handleWilderness}>Local Wilderness</button>
+        <div className={styles['village-buttons-container']}>
+          <button
+            className={styles['village-button']}
+            value="tavern"
+            onClick={(event) => handleVillageLocationChange(event)}
+          >
+          Tavern
+          </button>
+          <button
+            className={styles['village-button']}
+            value="church"
+            onClick={(event) => handleVillageLocationChange(event)}
+          >
+            Church
+          </button>
+          <button
+            className={styles['village-button']}
+            value="shop"
+            onClick={(event) => handleVillageLocationChange(event)}
+          >
+            Shop
+          </button>
+          <button
+            className={styles['village-button']}
+            onClick={handleWilderness}
+          >
+            Combat
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default VillagePage;
