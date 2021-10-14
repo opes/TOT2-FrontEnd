e4c8742847a5f7814c611644145d66cf34532534
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-10-14 13:59:50 -0700

    WIP creating enemy stats display component

---
+++ src/components/display-components/EnemyStats.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './PlayerScroll.css';
+import PropTypes from 'prop-types';
+import { enemyList } from '../../data/enemy-list';
+
+const EnemyStats = ({ name, }) => {
+  const enemySrc = { goblin, hobgoblin };
+}
